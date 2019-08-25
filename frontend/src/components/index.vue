04e179917cca817ece7e1bd69ed49888e9c0eed5
MilePrastalo <mile.prastalo97@gmail.com>
2019-08-25 22:49:30 +0200

    some styling

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
         <navBar></navBar>
+        <v-parallax
+                src="background.jpg"
+                height="450"
+                jumbotron
+        >
             <v-container >
                 <v-row align="center" class="largeHeight">
-                    <v-col  md="6" sm="12" xs="10"  align="center">
-                        <v-btn @click="login" class="firstButton">Login</v-btn>
+                    <v-col md="3"></v-col>
+                    <v-col  md="3" sm="12" xs="10"  align="center">
+                        <v-btn large color="primary" @click="login" class="firstButton">Login</v-btn>
                     </v-col>
-                    <v-col   md="6" sm="12" xs="10" align="center">
-                        <v-btn @click="register">Register</v-btn>
+                    <v-col   md="3" sm="12" xs="10" align="center">
+                        <v-btn large color="primary" @click="register">Register</v-btn>
                     </v-col>
+                    <v-col md="3"></v-col>
                 </v-row>
             </v-container>
+        </v-parallax>
         </div>
 </template>
 
